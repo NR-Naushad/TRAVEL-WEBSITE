dc4dabd4976ce92df7510a530f929dbe310a5a66
NR-Naushad <114635317+NR-Naushad@users.noreply.github.com>
2023-07-13 14:21:26 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -13,13 +13,13 @@ function send_mail($to,$subject,$message){
           $mail->isSMTP(); 
           $mail->Host = 'Nr.Travel.com'; 
           $mail->SMTPAuth = true;
-          $mail->Username = "support@travel.com"; 
+          $mail->Username = "naushadraja557@gmail.com"; 
           $mail->Password = "travel0p455w0rd";
           $mail->SMTPSecure = 'ssl';  
           $mail->Port = 465; 
-          $mail->setFrom("support@travel.com", "Travel Point");
+          $mail->setFrom("naushadraja557@gmail.com", "NR-Naushad");
           $mail->addAddress($to);
-          $mail->addReplyTo("support@travel.com","Travel Point");
+          $mail->addReplyTo("aushadraja557@gmail.com","NR-Naushad");
           $mail->isHTML(true); 
           $mail->Subject = $subject;
           $mail->Body    = $message;
